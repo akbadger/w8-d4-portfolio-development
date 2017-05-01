f3e01059fb2b398f1ae734cbb662f215d612eaef
Aubrey Badger <akbadger@gmail.com>
2017-04-30 21:33:38 -0400

    added homepage to package json, created production build

--- portfolio/src/Work.js
+++ portfolio/src/Work.js
@@ -5,13 +5,13 @@ class Work extends Component {
   render() {
     return <div>
                <div className="columns">
-                    <WorkSample title="TURN" screenshot="/img/turn.png" description="Iron Yarn final project in tandem with New Leaf, New Life: An employment platform connecting workers who have been previously incarcerated and employers who are willing to hire them.  Languages/libraries used: HTML5, CSS3, Bootstrap, Javascript, React" github="https://github.com/dembrey2/leafy" link=""/>
+                    <WorkSample title="TURN" screenshot="/img/turn.png" description="Iron Yarn final project in tandem with New Leaf, New Life: An employment platform connecting workers who have been previously incarcerated and employers who are willing to hire them.  Languages/libraries used: HTML5, CSS3, Bootstrap, Javascript, React" github="https://github.com/dembrey2/leafy" link="https://turn-newleaf.herokuapp.com"/>
               </div>
               <div className="columns">
-                    // <WorkSample title="CREATE & DESIGN CO" screenshot="/img/create-and-design.png" description="Customized blogging platform.  Languages/libraries used: HTML5, CSS3, Bootstrap" github="https://github.com/akbadger/w2-d4-recreate-surf-and-paddle" link=""/>
+                    <WorkSample title="CREATE & DESIGN CO" screenshot="/img/create-and-design.png" description="Customized blogging platform.  Languages/libraries used: HTML5, CSS3, Bootstrap" github="https://github.com/akbadger/w2-d4-recreate-surf-and-paddle" link="https://akbadger.github.io/w2-d4-recreate-surf-and-paddle/"/>
                 </div>
               <div className="columns">
-                    <WorkSample title="TODO" screenshot="./img/todolist.png" description="Minimalist to-do list web application. Languages/libraries used: HTML5, CSS3, Bootstrap, Javascript" github="https://github.com/akbadger/w6-d4-finish-todo-app" link=""/>
+                    <WorkSample title="TODO" screenshot="./img/todolist.png" description="Minimalist to-do list web application. Languages/libraries used: HTML5, CSS3, Bootstrap, Javascript" github="https://github.com/akbadger/w6-d4-finish-todo-app"/>
               </div>
             </div>
                   
--- portfolio/src/WorkSample.js
+++ portfolio/src/WorkSample.js
@@ -14,7 +14,7 @@ class WorkSample extends Component {
     return <div>
                     <div className="column is-half is-offset-one-quarter has-text-centered">
                          <img src={this.props.screenshot} alt="sample screenshot" className="screenshot"/>
-                        <span>{this.props.title}<a target="_blank" href={this.props.github}><i className="fa fa-github github-small" aria-hidden="true"></i></a></span><br/> {this.props.description}<br/>
+                        <span><a target="_blank" href={this.props.link}>{this.props.title}</a><a target="_blank" href={this.props.github}><i className="fa fa-github github-small" aria-hidden="true"></i></a></span><br/> {this.props.description}<br/>
                     </div>
                 </div> 
   }
